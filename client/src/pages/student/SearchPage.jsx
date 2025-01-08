dfb1170f8546e7da19a603a4a8e0f863720927f0
Divyesh Jodhani <142737551+Divyeshjodhani@users.noreply.github.com>
2025-01-08 17:26:15 +0100

    Update_Filter

--- client/src/pages/student/SearchPage.jsx
+++ client/src/pages/student/SearchPage.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "./Filter";
-import Courses from "./Courses";
-import CourseProgress from "./CourseProgress";
 import SearchResult from "./SearchResult";
+import { useGetSearchCourseQuery } from "@/features/api/courseApi";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Link, useSearchParams } from "react-router-dom";
+import { AlertCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const SearchPage = () => {
-  const isLoading = false;
-  const isEmpty = false;
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
+  const [selectedcategories, setSelectedCatgories] = useState([]);
+  const [sortByPrice, setSortByPrice] = useState("");
+
+  const { data, isLoading } = useGetSearchCourseQuery({
+    searchQuery: query,
+    categories: selectedcategories,
+    sortByPrice,
+  });
+
+  const isEmpty = !isLoading && data?.courses.length === 0;
+
+  const handleFilterChange = (categories, price) => {
+    setSelectedCatgories(categories);
+    setSortByPrice(price);
+  };
   return (
-    <div className="max-w-7xl mx-auto p-4 md:p-8">
+    <div className="max-w-7xl mx-auto p-4 md:p-8 mt-5">
       <div className="my-6">
-        <h1>result for "html"</h1>
+        <h1 className="font-bold text-xl md:text-2xl">result for "{query}"</h1>
         <p>
           Showing results for{" "}
-          <span className="text-blue-800 font-bold italic">
-            Frontend developer
-          </span>
+          <span className="text-blue-800 font-bold italic">{query}</span>
         </p>
       </div>
       <div className="flex flex-col md:flex-row gap-10">
-        <Filter />
+        <Filter handleFilterChange={handleFilterChange} />
         <div className="flex-1">
           {isLoading ? (
             Array.from({ length: 3 }).map((_, idx) => (
@@ -28,7 +44,9 @@ const SearchPage = () => {
           ) : isEmpty ? (
             <CourseNotFound />
           ) : (
-            [1, 2, 3].map((course, idx) => <SearchResult key={idx} />)
+            data?.courses?.map((course) => (
+              <SearchResult key={course._id} course={course} />
+            ))
           )}
         </div>
       </div>
